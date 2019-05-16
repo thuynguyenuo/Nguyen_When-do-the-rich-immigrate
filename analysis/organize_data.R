59dd00c79b21a3eaadd0b409b29241f7354de442
Aaron Gullickson <aarong@uoregon.edu>
2019-05-16 11:36:14 -0700

    processing visa data

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -8,3 +8,19 @@
 
 #source in any useful functions
 source("useful_functions.R")
+
+library(readr)
+
+visa <- read_csv("input/tabula-FY18AnnualReport - TableV-Part3.csv",
+                 skip = 4,
+                 col_names = c("state","employ_creation","target_areas",
+                               "region_pilot","region_target","total",
+                               "employ_total","grand_total"))
+
+visa <- subset(visa, state!="TOTAL" & !grepl("Region", state) 
+               & state!="#NAME?" & state!="Africa" & state!="Grand Totals",
+               select=c("state","employ_total"))
+
+visa$year <- 2018
+
+total_visa <- rbind(visa, new_visa)
